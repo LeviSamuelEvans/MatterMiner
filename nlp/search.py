e958d54f12abcb5db64487aedbffeb91b90dc2b6
Levi Evans <levi.samuel.evans@cern.ch>
2024-05-24 00:32:42 +0100

    feat: first working attempt

--- nlp/search.py
+++ nlp/search.py
@@ -2,20 +2,22 @@ import sqlite3
 from nlp.preprocess import preprocess_text
 from config import DATABASE
 
-def search_posts(query):
+def search_comments(query):
     conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
-    c.execute("SELECT message FROM posts")
-    posts = c.fetchall()
+    c.execute("SELECT body FROM comments")
+    comments = c.fetchall()
     conn.close()
 
     query_doc = preprocess_text(query)
 
     results = []
-    for post in posts:
-        post_doc = preprocess_text(post[0])
-        similarity = query_doc.similarity(post_doc)
-        results.append((post[0], similarity))
+    for comment in comments:
+        comment_doc = preprocess_text(comment[0])
+        similarity = query_doc.similarity(comment_doc)
+        results.append((comment[0], similarity))
 
     results = sorted(results, key=lambda x: x[1], reverse=True)
     return results
+
+# Path: nlp/preprocess.py
--- web/app.py
+++ web/app.py
@@ -1,5 +1,11 @@
+import sys
+import os
+
+# Add the project root directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask, request, render_template
-from nlp.search import search_posts
+from nlp.search import search_comments
 
 app = Flask(__name__)
 
@@ -10,5 +16,8 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     query = request.form['query']
-    results = search_posts(query)
+    results = search_comments(query)
     return render_template('results.html', query=query, results=results[:5])
+
+if __name__ == "__main__":
+    app.run(debug=True)
