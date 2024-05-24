157697abc496edc107ad530b4b5375cb76785d7a
Levi Evans <levi.samuel.evans@cern.ch>
2024-05-24 01:45:19 +0100

    feat: use vaderSentiment

--- nlp/preprocess.py
+++ nlp/preprocess.py
@@ -1,7 +1,11 @@
 import spacy
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-# larger model with word vectors
 nlp = spacy.load('en_core_web_md')
+sentiment_analyzer = SentimentIntensityAnalyzer()
 
 def preprocess_text(text):
     return nlp(text)
+
+def analyze_sentiment(text):
+    return sentiment_analyzer.polarity_scores(text)
