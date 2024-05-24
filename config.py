3326e52e8de7bec711943fb21ac920c621e419a4
Levi Evans <levi.samuel.evans@cern.ch>
2024-05-24 04:22:23 +0100

    some cleanup

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Configuration variables
 
-REDDIT_CLIENT_ID = "xxK4pV4thiZKC65Naw6lhg"  # Replace with your actual client ID
+REDDIT_CLIENT_ID = "xxK4pV4thiZKC65Naw6lhg"
 REDDIT_CLIENT_SECRET = (
-    "RTmZNsaTsrap_xYsIkNePkwhk1qnWg"  # Replace with your actual client secret
+    "RTmZNsaTsrap_xYsIkNePkwhk1qnWg"
 )
-REDDIT_USER_AGENT = "MatterMiner by /u/HiggsOG"  # Replace with your actual user agent
+REDDIT_USER_AGENT = "MatterMiner by /u/HiggsOG" 
 DATABASE = "reddit.db"
